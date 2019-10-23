bb36794260705f256c9ae53807704eaa22430c8e
vsilva2019 <victor29092015@gmail.com>
2019-10-23 17:38:27 -0200

    logs

---
+++ teste log/log/src/main/java/Teste/log/App.java
@@ -0,0 +1,13 @@
+package Teste.log;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
