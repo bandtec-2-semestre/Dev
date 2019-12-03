440c9c0af04bb0adbefa5a98819c4b7f7eabe0ae
esteves-esta <26911877+esteves-esta@users.noreply.github.com>
2019-12-02 23:11:16 -0300

    Adicionei script neste repositorio

---
+++ ScriptInstalacao/ScriptInstalacao-zsh.sh
@@ -0,0 +1,34 @@
+#!/bin/zsh
+
+echo "A instalação de seu software de monitoração securIT começou..."
+
+cd ~
+
+mkdir securit
+
+cd securit
+
+cp $HOME/Área\ de\ Trabalho/securIT/Dev/securit.jar securit.jar
+
+chmod +x securit.jar
+
+which java
+
+echo "..."
+
+if [ $? = 0 ]
+	then echo "Prosseguindo a instalação"
+	else sudo add-apt-repository ppa:webupd8team/java
+fi
+
+
+echo java -jar $HOME/securit/securit.jar > securIT.txt
+
+chmod +x $HOME/securit/securit.jar
+
+sudo cp $HOME/securit/securit.jar /usr/local/bin/securit
+
+echo "..."
+
+echo "A instalação foi executada com sucesso. Apenas entre na pasta $HOME/securit e clique duas vezes no arquivo securit.jar"
+
